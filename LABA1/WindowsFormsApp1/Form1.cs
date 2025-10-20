6b0885eddcfc73a76d6c3163f6c5db8ce3b541a4
Ivan Pasternak <ivlpasternak@gmail.com>
2025-10-20 23:51:53 +0300

    yraaaaaaaaaaa

--- LABA1/WindowsFormsApp1/Form1.cs
+++ LABA1/WindowsFormsApp1/Form1.cs
@@ -9,7 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Windows.Forms.VisualStyles;
+// РЕШЕНИЕ ЗАДАЧ НОМЕР: 18, 19, 20, 57
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -54,5 +55,114 @@ namespace WindowsFormsApp1
         {
             
         }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string number = textBox2.Text;
+            string value = textBox1.Text;
+            string rez = "";
+
+            List<string> numbs = value.Split(' ').ToList();
+            List<int> values = new List<int>();
+
+            for (int i = 0; i<numbs.Count;i++ )
+            {
+                if (int.TryParse(numbs[i],out int n))
+                {
+                    if (n%10==Convert.ToInt32(number) && n>0)
+                    {
+                        while(values.IndexOf(n) != -1)
+                        {
+                            values.Remove(n);
+                        }
+                        values.Add(n);
+                        
+                    }    
+                }
+            }
+            
+            foreach (int i in values)
+            {
+                rez += " " + Convert.ToString(i);
+            }
+            label9.Text= rez;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            string rez = "";
+
+            List<string> numbs = value.Split(' ').ToList();
+            List<int> values = new List<int>();
+            for (int i = 0; i < numbs.Count; i++)
+            {
+                if (int.TryParse(numbs[i], out int n))
+                {
+                    if (n >= 10 && n <= 99)
+                    {
+                        values.Add(n);
+                    }
+
+                }
+            }
+            values.Sort();
+            foreach (int i in values)
+            {
+                rez += " " + Convert.ToString(i);
+            }
+            label11.Text= rez; 
+    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            string rez = "";
+            // 22 34 32 56
+            List<string> numbs = value.Split(' ').ToList();
+            List<int> values = new List<int>();
+            List<int> ends = new List<int>();
+
+            foreach (string i in numbs)
+            {
+                if (int.TryParse(i, out int n))
+                {
+                    while (ends.IndexOf(n%10) != -1)
+                    {
+                        ends.Remove(n%10);
+                    }
+                    ends.Add(n % 10);
+                }
+            }
+            int maxi = -10000000;
+
+            foreach (int end  in ends)
+            {
+                foreach (string i in numbs)
+                {
+                    if (int.TryParse(i, out int n))
+                    {
+                        if (n%10== end)
+                        {
+                            if (maxi < n) maxi = n;
+                        }    
+                    }
+                }
+                values.Add(maxi);
+                maxi = -10000000;
+            }
+            ends.Sort();
+            foreach (int i in values)
+            {
+                rez += " " + Convert.ToString(i);
+            }
+            label13.Text = rez;
+        }
+
     }
 }
---
+++ LABA1/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            List<string> mas_value = value.Split(' ').ToList();
+            string rez = "";
+
+
+            for (int i = 0; i < mas_value.Count - 1; i++)
+            {
+                for (int j = 0; j < mas_value.Count - 1 - i; j++)
+                {
+                    if (mas_value[j].Length > mas_value[j + 1].Length)
+                    {
+                        string t1 = mas_value[j];
+                        mas_value[j] = mas_value[j + 1];
+                        mas_value[j + 1] = t1;
+                    }
+                }
+            }
+
+            for (int i = 0; i<mas_value.Count-1; i++)
+            {
+                if (mas_value[i].Length == mas_value[i+1].Length)
+                {
+                    int result = string.Compare(mas_value[i], mas_value[i + 1], StringComparison.Ordinal);
+                    if (result > 0)
+                    {
+                        string t1 = mas_value[i];
+                        mas_value[i] = mas_value[i + 1];
+                        mas_value[i + 1] = t1;
+                    }    
+                }
+            }
+
+            foreach (string a in  mas_value)
+            {
+                rez += " " + a;
+            }
+            label3.Text = rez;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            int k = Convert.ToInt32(textBox2.Text);
+            List<string> mas_value = value.Split(' ').ToList();
+            List<string> mas_rez = new List<string>();
+            string rez = "";
+
+            for (int i = 0; i<mas_value.Count;i++)
+            {
+                if (mas_value[i].Length == k)
+                {
+                    if (char.IsDigit(mas_value[i][k-1]))
+                    {
+                        mas_rez.Add(mas_value[i]);
+                    }
+                }
+            }
+            mas_rez.Sort();
+            foreach (string a in mas_rez)
+            {
+                rez += " " + a;
+            }
+            label6.Text = rez;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            List<string> mas_value = value.Split(' ').ToList();
+            HashSet<int> mn_ends = new HashSet<int>();
+            List<string> mas_rez = new List<string>();
+            string rez = "";
+
+            foreach (string ends in mas_value)
+            {
+                mn_ends.Add((int)ends[0]);
+            }
+            List<int> mas_ends = mn_ends.ToList();
+            mas_value.Sort();
+            int maxi = -1;
+            foreach (int end in mas_ends)
+            {
+                string t = "";
+                for (int i = 0; i < mas_value.Count; i++)
+                {
+                    if (end == (int)mas_value[i][0])
+                    {
+                        if (maxi < mas_value[i].Length)
+                        {
+                            maxi = mas_value[i].Length;
+                            t = mas_value[i];
+                        }
+                    }
+                }
+                mas_rez.Add(t);
+                maxi = -1;
+            }
+            foreach (string a in mas_rez)
+            {
+                rez += " " + a;
+            }
+            label8.Text = rez;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            List<string> mas_value = value.Split(' ').ToList();
+            HashSet<int> mn_l_mass = new HashSet<int>();
+            List<string> mas_rez = new List<string>();
+            string rez = "";
+
+            foreach (string i in  mas_value)
+            {
+                mn_l_mass.Add(i.Length);  
+            }
+            List<int> mas_l_mass = mn_l_mass.ToList();
+            mas_l_mass.Sort();
+            mas_l_mass.Reverse();
+            List<string> t = new List<string>();
+            foreach (int l in mas_l_mass)
+            {
+                foreach (string str in mas_value)
+                {
+                    if (str.Length == l)
+                    {
+                        t.Add(str);
+                    }
+                }
+                t.Sort();
+                mas_rez.Add(t[0]);
+                t.Clear();
+            }
+            foreach (string a in mas_rez)
+            {
+                rez += " " + a;
+            }
+            label10.Text = rez;
+        }
+    }
+}
