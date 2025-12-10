ad7262cfecee96aec9e80110ff93715142ec1df0
Ivan Pasternak <ivlpasternak@gmail.com>
2025-12-10 21:53:51 +0300

    classComplex

---
+++ labaclass/classiiii/ComplexNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace ComplexNumbers
+{
+    public class ComplexNumber
+    {
+        private double real;
+        private double imaginary;
+
+        public ComplexNumber()
+        {
+            real = 0;
+            imaginary = 0;
+        }
+        public ComplexNumber(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public void Input(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public string CreateStringNumber ()
+        {
+            if (imaginary >= 0)
+                return $"z = {real} + {imaginary}i";
+            else 
+                return $"z = {real} - {Math.Abs(imaginary)}i";
+        }
+
+        public ComplexNumber Power(int n)
+        {
+            double r = Math.Sqrt(real * real + imaginary * imaginary);
+            double phi = Math.Atan2(imaginary, real);
+
+            double rPow = Math.Pow(r, n);
+            double newPhi = n * phi;
+
+            double newReal = rPow * Math.Cos(newPhi);
+            double newImaginary = rPow * Math.Sin(newPhi);
+
+            return new ComplexNumber(newReal, newImaginary);
+        }
+
+        public double Real
+        {
+            get { return real; }
+            set {  real = value; }
+        }
+
+        public double Imaginary
+        {
+            get { return imaginary; }
+            set { imaginary = value; }
+        }
+    }
+}
---
+++ labaclass/classiiii/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComplexNumbers;
+
+namespace classiiii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public ComplexNumber complex = new ComplexNumber();
+        private bool CheckDouble(string[] valueMas)
+        {
+            int k = 0;
+            foreach (string s in valueMas)
+            {
+                if (double.TryParse(s,out _))
+                {
+                    k++;
+                }
+                else
+                    return false;
+            }
+            if (k == valueMas.Length)
+                return true;
+            return false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string[] valueMas = textBox1.Text.Split();
+            if (valueMas.Length < 3 )
+            {
+                if (valueMas.Length == 2)
+                { 
+                    if (CheckDouble(valueMas))
+                    {
+                        complex.Input(Convert.ToDouble(valueMas[0]), Convert.ToDouble(valueMas[1]));
+                        label4.Text = complex.CreateStringNumber();
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Ошибка, введено больше 2 символов");
+                textBox1.Clear();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strDegree = textBox2.Text;
+            int degree;
+            if (int.TryParse(textBox2.Text,out degree))
+            {
+                if (degree > 0)
+                {
+                    ComplexNumber rez_complex = complex.Power(degree);
+                    label5.Text = rez_complex.CreateStringNumber();
+                }
+            }
+        }
+    }
+}
